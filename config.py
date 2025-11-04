01570991414b21863bfc83bce5d02f8710e0ca55
LuquitaDoidao <lucasamarantelima7@gmail.com>
2025-11-04 06:44:03 -0300

    aaa

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ SECRET_KEY = 'CHAVE-SUPER-SECRETA'  # Chave secreta para criptografia
 DEBUG = True
 # Configurações do banco de dados Firebird
 DB_HOST = 'localhost'
-DB_NAME = r'C:\Users\User\Downloads\Nova-API-LIBRIS-main\Nova-API-LIBRIS-main\BANCO_LIBRIS.FDB'
-#UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static/uploads')
+DB_NAME = '..\BANCO_LIBRIS.FDB'
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
 
 
